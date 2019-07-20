810e67f9b3e7e9519ec4cc07ce5f8fab7867e7c0
Petar Dobrinov <petardobrinov95@gmail.com>
2019-07-20 16:19:11 +0300

    Blackjack working

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,5 @@
 import random
+import os
 playing = True
 suits = ['Hearts', 'Spades', 'Diamonds', 'Clubs']
 ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
@@ -30,21 +31,18 @@ class Hand:
         self.aces = 0
     def add_card(self, card):
         self.cards.append(card)
-        self.value += values[card[1]]
-    def adjust_for_ace(self):
-        for card in self.cards:
-            if 'A' in card:
-                self.aces += 1
-                if self.value + 11 > 21:
-                    self.value -= 10
+        if card[1] == 'A' and self.value + values[card[1]] > 21 and self.value + 1 <= 21:
+            self.value +=1
+        else:
+            self.value += values[card[1]]
 class Chip:
-    def __init__(self, bet):
+    def __init__(self):
         self.total = 100
-        self.bet = bet
+        self.bet = 0
     def win_bet(self, price):
-        self.total += self.bet
+        self.total += price
     def lose_bet(self, price):
-        self.total -= self.bet
+        self.total -= price
 def take_bet():
     global bet
     while True:
@@ -58,63 +56,162 @@ def take_bet():
             break
 def hit(deck,hand):
     hand.add_card(deck.deal())
-    hand.adjust_for_ace()
 def hit_or_stand(deck, hand):
     global playing
-    while playing:
+    while True:
+        try:
+            ask = raw_input("Hit or Stand? (h/s) ")
+            if ask != 'h':
+                if ask != 's':
+                    raise TypeError
+        except:
+            print 'Bad input!'
+            continue
+        else:
+            break
+    if ask == 'h':
+        hit(deck, hand)
+        return 'h'
+    else:
+        return 's'
+
+def show_some(player, dealer):
+    print "Player's hand: "
+    for card in player.cards:
+        print '{} of {}'.format(card[1], card[0])
+    print "{} points".format(player.value)
+    print "\nDealer's hand: "
+    for card in dealer.cards[1:]:
+        print '{} of {}'.format(card[1], card[0])
+
+def show_all(player, dealer):
+    print "Player's hand: "
+    for card in player.cards:
+        print '{} of {}'.format(card[1], card[0])
+    print "{} points".format(player.value)
+    print "\nDealer's hand: "
+    for card in dealer.cards:
+        print '{} of {}'.format(card[1], card[0])
+    print "{} points".format(dealer.value)
+
+def player_busts(balance, bet):
+    balance.lose_bet(bet)
+    print 'You busted!'
+
+def player_wins(balance):
+    balance.win_bet(bet)
+    print 'You win!'
+
+def dealer_busts(balance):
+    balance.win_bet(bet)
+    print 'Dealer busted!'
+
+def dealer_wins(balance):
+    balance.lose_bet(bet)
+    print 'Dealer wins'
+
+def push():
+    print 'TIE'
+def clear():
+    os.system('clear')
+def replay():
+    while True:
+        try:
+            ask = raw_input('Your balance is {}$\nPlay again? y/n: '.format(balance.total))
+            if ask != 'y':
+                if ask != 'n':
+                    raise TypeError
+            if ask == 'y':
+                return 'y'
+            elif ask == 'n':
+                return 'n'
+
+        except:
+            clear()
+            print 'Bad input!'
+            continue
+        else:
+            break
+def new_game():
+    while True:
+        try:
+            ask = raw_input('Play a new game? y/n ')
+            if ask != 'y':
+                if ask != 'n':
+                    raise TypeError
+            if ask == 'y':
+                return 'y'
+            else:
+                return 'n'
+        except:
+            print 'Bad input!'
+            continue
+        else:
+            break
+while True:
+    balance = Chip()
+    while True:
+        clear()
+        print 'Your balance is {}$'.format(balance.total)
         while True:
             try:
-                ask = raw_input("Hit or Stand? ")
-                if ask != 'Hit' or ask != 'Stand':
-                    raise TypeError
+                bet = int(input('The game begins!\nPlease take a bet '))
+                if bet > balance.total or bet < 0:
+                    raise ValueError
             except:
-                print 'Bad input!'
+                print 'Bad betting, try again: '
                 continue
             else:
                 break
-        if ask == 'Hit':
-            hit()
+
+        clear()
+        player = Hand()
+        dealer = Hand()
+        deck = Deck()
+        deck.shuffle()
+        dealer.add_card(deck.deal())
+        dealer.add_card(deck.deal())
+        player.add_card(deck.deal())
+        player.add_card(deck.deal())
+        show_some(player, dealer)
+        while playing:
+            print '\n'
+            res = hit_or_stand(deck,player)
+            if res == 'h':
+                clear()
+                show_some(player, dealer)
+            if player.value > 21:
+                print "\n"
+                player_busts(balance, bet)
+                break
+            if res == 's':
+                while dealer.value < 17:
+                    hit(deck, dealer)
+                clear()
+                show_all(player, dealer)
+                print "\n"
+                if 21 - player.value < 21 - dealer.value:
+                    player_wins(balance)
+                    break
+                elif dealer.value > 21:
+                    dealer_busts(balance)
+                    break
+                elif 21 - dealer.value < 21 - player.value:
+                    dealer_wins(balance)
+                    break
+                elif dealer.value == player.value:
+                    push()
+                    break
+        if balance.total <= 0:
+            break
+        repl = replay()
+        if repl == 'y':
+            continue
         else:
             break
-def show_some(player, dealer):
-    print "Player's hand: {} {}".format(player.cards, player.value)
-    print "Dealer's hand: NA, {}".format(dealer.cards[1:])
-def show_all(player, dealer):
-    print "Player's hand: {} {}".format(player.cards, player.value)
-    print "Dealer's hand: {} {}".format(dealer.cards, dealer.value)
-
-def player_busts(player, dealer):
-    player.lose_bet(bet)
-    if player.total > 21:
-        return 'You busted!'
-
-def player_wins(player,dealer):
-    if 21 - player.total < 21 - dealer.total:
-        player.win_bet(bet)
-        return 'You win!'
-
-def dealer_busts(player, dealer):
-    player.win_bet(bet)
-    if dealer.total > 21:
-        return 'Dealer busted!'
-
-def dealer_wins(player, dealer):
-    player.lose_bet(bet)
-    if 21 - dealer.total < 21 - player.total:
-        return 'Dealer wins'
-
-def push(player, dealer):
-    if dealer.total == player.total:
-        return 'TIE'
-
-while True:
-    bet = int(input('The game begins!\nPlease take a bet '))
-    player = Hand()
-    dealer = Hand()
-    deck = Deck()
-    deck.shuffle()
-    dealer.add_card(deck.deal())
-    dealer.add_card(deck.deal())
-    player.add_card(deck.deal())
-    player.add_card(deck.deal())
-    show_some(player, dealer)
+    print 'You are out of money'
+    new = new_game()
+    if new == 'y':
+        continue
+    else:
+        break
